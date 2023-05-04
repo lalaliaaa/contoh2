60f69dc3c2f91156a05eaa8dd70561ca24e287b9
lalaliaaa <132461721+lalaliaaa@users.noreply.github.com>
2023-05-04 08:08:00 +0700

    Update index.php

--- index.php
+++ index.php
@@ -16,8 +16,8 @@
             <header>
                 <h2>About Me</h2>
                 <figure>
-                    <img src="https://storage.cloud.google.com/my-project-nurrapi-385700.appspot.com/fotodiri%20(2).jpeg?authuser=2" style="width:500px;height:500px;">
-                    width: 100px;                                                                                                                
+                    <img src="https://storage.cloud.google.com/my-project-nurrapi-385700.appspot.com/fotodiri%20(2).jpeg?authuser=2" style="width:300px;height:300px;">
+                                                                                                                                    
                 </figure> 
             </header>
             <section>
