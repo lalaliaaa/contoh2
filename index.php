519845da66c79d47487077131ff07aeec8693a61
lalaliaaa <132461721+lalaliaaa@users.noreply.github.com>
2023-05-04 07:00:30 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+  <meta charset="utf-8">
+  <head>
+  <title>Nur Rapi Ria Amalia</title>
+  <meta name="viewport content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="styles.css"> 
+  </head>
+  <body>
+    <header>
+        <div id="header">
+
+    
+    <article>
+        <article class="profile">
+            <header>
+                <h2>About Me</h2>
+                <figure>
+                    <img src="fotodiri (2).jpeg">
+                </figure> 
+            </header>
+            <section>
+                <table>
+                    <tr>
+                        <th>Nama </th>
+                        <td>Nur Rapi Ria Amalia</td>
+                    </tr>
+                    <tr>
+                        <th>Universitas </th>
+                        <td>Universitas Gunadarma</td>
+                    </tr>
+                    <tr>
+                        <th>Jurusan </th>
+                        <td>Sistem Informasi<td>
+                    </tr>
+                 
+                    <tr>
+                        <th>Learning Path </th>
+                        <td>Cloud Computing</td>
+                    </tr>
+                </table> 
+            </section>
+        </article>
+    
+    </main>
+    <br>
+    <footer>
+        &copy; 2023, Nur Rapi Ria Amalia | CC - Bangkit2023
+    </footer>
+
+  </body>
+</html>
